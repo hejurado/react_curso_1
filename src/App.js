1f003665d6c7b3a130ddb812137c89299e7341c9
hjs <hejurado@gmail.com>
2021-07-04 16:30:26 -0500

    arreglos en el texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.hello worlld 
+          Edit <code>src/App.js</code> and save to reload this is my hello world 
         </p>
         <a
           className="App-link"
